6ac5fe21ec058a4bcd15731889a9ce349ce6d427
csycsy123123 <1137549530@qq.com>
2021-12-22 16:05:53 +0800

    init

---
+++ AddStudent.java
@@ -0,0 +1,31 @@
+package com.atguigu.jdbc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/AddStudent")
+public class AddStudent extends HttpServlet {
+	
+	StudentService studentService = new StudentService();
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      PrintWriter out = response.getWriter();
+	  //构造对象
+	  Student student = new Student();
+	  student.setId(Integer.valueOf(request.getParameter("id")));
+	  student.setName(request.getParameter("name"));
+	  student.setAge(Integer.valueOf(request.getParameter("age")));
+	  //执行
+      String result = studentService.addStudent(student);
+
+      out.println("{\"result\":" + result + "}");
+      out.flush();
+      out.close();
+   }
+}
